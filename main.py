7c7d6f3cdbf50b0e4f3006307d28e730e0f35b4c
Kaiisfly <kanseladams@gmail.com>
2023-09-30 20:06:05 +0800

    Remove more files

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 # Save File path
-datapath = "C:/Users/kanse/PycharmProjects/Melbourne_Housing_Machine_Learning/melb_data.csv"
+datapath = "melb_data.csv"
 # Read Data
 data = pd.read_csv(datapath)
 
 # Show summary of data
 data.describe()
-
 
 
